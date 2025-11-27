e0a53e0494ce7a638e9f037be96a51738b09d0b0
NTB - MAVELLIUM <ntb-mavellium@192.168.0.52>
2025-11-27 11:38:30 -0300

    GoogleTagMenager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,12 @@ import Script from "next/script";
 import "./globals.css";
 
 const montserrat = Montserrat({ subsets: ["latin"], weight: ["400", "700"] });
+const GTM_ID = "GTM-5D4N3KXM";
 
 export const metadata: Metadata = {
   title: "Instituto do Sorriso",
   description: "Transformamos sorrisos.",
+<<<<<<< HEAD
   icons: {
     icon: [
       // Light mode
@@ -28,25 +30,40 @@ export const metadata: Metadata = {
     ],
   },
   manifest: "/site.webmanifest",
+=======
+>>>>>>> d4c406f (GoogleTagMenager)
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-br">
       <body className={`${montserrat.className} antialiased font-sans`}>
-        {/* Google Analytics */}
-        <Script
-          strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=G-HJHT00LVC8"
-        />
-        <Script id="gtag-init" strategy="afterInteractive">
+        
+        {/* GTM Head */}
+        <Script id="gtm-script" strategy="afterInteractive">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-HJHT00LVC8');
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),
+                dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
           `}
         </Script>
+
+        {/* GTM noscript */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
 
         {children}
       </body>
